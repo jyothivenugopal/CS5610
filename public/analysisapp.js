444efd53641df9c7897ab9f4b0b8c941b31b5484
jyothiv91 <jv_604@yahoo.co.in>
2014-12-08 10:17:43 -0500

    more changes

---
+++ public/analysisapp.js
@@ -0,0 +1,72 @@
+var routerApp = angular.module('routerApp', ['ui.router']);
+
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'partial-home-analysis.html'
+        })
+
+        /*.state('carsnearme', {
+            url: '/carsnearme',
+            templateUrl: 'carsnearme.html'
+        })*/
+        
+        // nested list with custom controller
+        .state('home.list', {
+            url: '/gohere',
+            templateUrl: 'partial-home-list-analysis.html',
+            /*controller: function($scope) {
+                $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+                console.log("hi from new controller");
+                
+            }*/
+        })
+        
+        // nested list with just some random string data
+        .state('home.paragraph', {
+            url: '/paragraph',
+            template: 'I could sure use a drink right now.'
+        })
+        
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+            url: '/about',
+            views: {
+                '': { templateUrl: 'partial-about.html' },
+                'columnOne@about': { template: 'Look I am a column!' },
+                'columnTwo@about': { 
+                    templateUrl: 'table-data.html',
+                    controller: 'scotchController'
+                }
+            }
+            
+        });
+        
+});
+
+routerApp.controller('scotchController', function($scope) {
+    
+    $scope.message = 'test';
+   
+    $scope.scotches = [
+        {
+            name: 'Macallan 12',
+            price: 50
+        },
+        {
+            name: 'Chivas Regal Royal Salute',
+            price: 10000
+        },
+        {
+            name: 'Glenfiddich 1937',
+            price: 20000
+        }
+    ];
+    
+});
